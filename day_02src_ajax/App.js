908c160b144d58e7a095095082d0f8fd8cb8fab7
liubo <2906213232@qq.com>
2020-04-28 21:25:22 +0800

    更新1

---
+++ day_02src_ajax/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Admin from './pages/Admin/Admin'
+import login from './pages/Login/Login'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+       Admin...
+      </div>
+    )
+  }
+}
